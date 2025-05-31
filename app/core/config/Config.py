4ace68f8a4f9804c8f64387116ae2ceb9b60194a
Julio Adrian Ferreyra Tello <julioferreyra.dev@gmail.com>
2025-05-31 02:00:54 +0000

    created and changed the config file to create and return a config class

--- app/core/config/Config.py
+++ app/core/config/Config.py
@@ -1,2 +1,18 @@
+from dataclasses import dataclass
+import os
+
+@dataclass
 class Config:
-    pass
+    app_name: str
+    app_version: str
+    debug: bool
+    
+    @staticmethod
+    def from_env():
+        return Config(
+            app_name=os.getenv("APP_NAME", "MyApp"),
+            app_version=os.getenv("APP_VERSION", "1.0.0"),
+            debug=os.getenv("DEBUG", "true").lower() == "true"
+        )
+        
+        
