4878dc56e5289d02e4940e737630c9329cf5d105
Julio Adrian Ferreyra Tello <julioferreyra.dev@gmail.com>
2025-06-29 22:52:54 +0000

    Enhance root endpoint with HTML response and application info; add logging for health check errors

--- app/app.py
+++ app/app.py
@@ -1,5 +1,7 @@
-import uvicorn
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
+
+from fastapi.responses import HTMLResponse
 from app.core.db.database import init_db
 from app.users.routes.user_routes import user_router
 from app.comments.routes.comment_routes import comment_router
@@ -7,11 +9,12 @@ from app.blogs.routes.blog_routes import blog_router
 from app.auth.routes.auth_routes import auth_router
 from app.tags.routes.tag_routes import tag_router
 from app.core.config.application_config import ApplicationConfig
+from app.utils.logger.application_logger import ApplicationLogger
+from app.core.dependencies.dependencies import provide_application_config
 
-
-
+_logger : ApplicationLogger = ApplicationLogger(name=__name__, log_to_console=False)
 # Gets the application global configuration
-application_config = ApplicationConfig()
+application_config: ApplicationConfig = provide_application_config()
 
 app = FastAPI(
     title=application_config.app_name,
@@ -23,12 +26,67 @@ app = FastAPI(
 init_db()
 
 
-@app.get(path="/")
-async def greet():
-    return {"Hello": "World"}
+@app.get(path="/", description="Root endpoint", tags=["status"], include_in_schema=False)
+async def home():
+    return HTMLResponse(f"""
+<!DOCTYPE html>
+<html>
+<head>
+    <title>FastAPI Application</title>
+    <style>
+        body {{
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+            background-color: #f4f4f9;
+            color: #333;
+        }}
+        h1 {{
+            color: #4CAF50;
+            text-align: center;
+            margin-top: 20px;
+        }}
+        p {{
+            text-align: center;
+            font-size: 18px;
+            margin: 10px 0;
+        }}
+        a {{
+            color: #4CAF50;
+            text-decoration: none;
+        }}
+        a:hover {{
+            text-decoration: underline;
+        }}
+        .container {{
+            max-width: 800px;
+            margin: 0 auto;
+            padding: 20px;
+            text-align: center;
+        }}
+    </style>
+</head>
+<body>
+    <div class="container">
+        <h1>Welcome to {app.title}!!</h1>
+        <p>Version: {app.version}</p>
+        <p>Welcome to the FastAPI application. Use the endpoints to interact with the service.</p>
+        <p>Check the <a href="/docs">API documentation</a> for more details.</p>
+        <p>For a quick overview, visit the <a href="/redoc">ReDoc documentation</a>.</p>
+    </div>
+</body>
+</html>
+""")
+@app.get(path="/info", description="Get application information", tags=["status"])
+async def get_info():
+    return {
+        "name": app.title,
+        "version": app.version,
+        "debug": app.debug
+    }
 
 
-@app.get(path="/health", description="Health check endpoint")
+@app.get(path="/health", description="Health check endpoint", tags=["status"])
 async def health_check():
     """Health check endpoint to verify the service is running.
     Returns:
@@ -38,6 +96,7 @@ async def health_check():
         # Here you can add any health check logic, like checking database connection
         return {"status": "ok"}
     except Exception as e:
+        _logger.log_error(message=f"Health check failed: {str(e)}")
         return {"status": "error", "detail": str(e)}
 
 app.include_router(user_router, prefix="/api/v1", tags=["users"])
@@ -46,6 +105,3 @@ app.include_router(blog_router, prefix="/api/v1", tags=["blogs"])
 app.include_router(tag_router, prefix="/api/v1", tags=["tags"])
 app.include_router(auth_router, prefix="/api/v1", tags=["auth"])
 
-if __name__ == "__main__" and application_config.debug:
-    uvicorn.run(app, host=application_config.host, port=int(
-        application_config.port), log_level="info")
