903854b0371b0239d589700f6eb698b0a8a4cff8
Julio Adrian Ferreyra Tello <julioferreyra.dev@gmail.com>
2025-07-02 16:22:40 +0000

    Refactor middleware imports and enhance Redis cache initialization for improved clarity and functionality

--- app/app.py
+++ app/app.py
@@ -17,10 +17,9 @@ from app.tags.routes.tag_routes import tag_router
 from app.core.config.application_config import ApplicationConfig
 from app.utils.logger.application_logger import ApplicationLogger
 from app.core.dependencies.dependencies import provide_application_config
-from app.core.middlewares.rate_limit_middleware import rate_limiter
+from app.core.middlewares import rate_limiter, clear_redis_cache, init_redis_cache
 from app.status.status_routes import app as status_router
 from app.admin.routes.admin_routes import admin_router
-from app.core.middlewares.cache_middleware import clear_redis_cache, init_redis_cache
 
 _logger: ApplicationLogger = ApplicationLogger(name=__name__, log_to_console=False)
 # Gets the application global configuration
--- app/core/middlewares/cache_middleware.py
+++ app/core/middlewares/cache_middleware.py
@@ -6,19 +6,19 @@ from fastapi_cache.backends.redis import RedisBackend
 
 from app.core.dependencies import provide_application_config
 from app.core.config.application_config import ApplicationConfig
-## Redis
-
 
 _application_config: ApplicationConfig = provide_application_config()
 
-async def init_redis_cache():
+
+async def init_redis_cache() -> bool:
     """Initialize Redis and FastAPI Cache."""
-    redis: aioredis.Redis = aioredis.from_url(_application_config.redis_url, encoding="utf-8", decode_responses=True)
-    FastAPICache.init(backend=RedisBackend(redis=redis), prefix="fastapi-cache")
+    redis: aioredis.Redis = aioredis.from_url(
+        _application_config.redis_url, encoding="utf-8", decode_responses=True)
+    FastAPICache.init(backend=RedisBackend(
+        redis=redis), prefix="fastapi-cache")
     return await redis.ping()
-     # Debugging line to confirm Redis connection
-    
+
+
 async def clear_redis_cache():
     """Clear the FastAPI Cache."""
-    value = await FastAPICache.clear()  # Clear cache on shutdown
-    print(f"Cache cleared: {value}")  # Debugging line to confirm cache clearing
+    await FastAPICache.clear()
