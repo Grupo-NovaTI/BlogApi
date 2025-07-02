652c8b6b07b7ca2cc4817b2c1c1cefa1fbc19d01
Julio Adrian Ferreyra Tello <julioferreyra.dev@gmail.com>
2025-07-02 19:54:36 +0000

    Refactor authentication routes to simplify login and registration logic by removing redundant exception handling

--- app/auth/routes/auth_routes.py
+++ app/auth/routes/auth_routes.py
@@ -1,16 +1,13 @@
 from starlette import status
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends
 from fastapi.security import OAuth2PasswordRequestForm
 from app.core.dependencies import AuthServiceDependency as AuthService
 from app.auth.schemas.token_response import TokenResponse
 from app.users.schemas.user_request import UserRequest
-from app.users.excepctions.user_exceptions import UserAlreadyExistsException, UserOperationException
-from app.auth.exceptions.auth_exceptions import InvalidUserCredentialsException, OperationFailedException
 auth_router = APIRouter(
     prefix="/auth",
     tags=["auth"]
 )
-
 
 @auth_router.post(path="/login", status_code=status.HTTP_200_OK, response_model=TokenResponse)
 async def login(auth_service: AuthService, form_data: OAuth2PasswordRequestForm = Depends()):
@@ -18,22 +15,9 @@ async def login(auth_service: AuthService, form_data: OAuth2PasswordRequestForm 
     Authenticate user with username and password.
     Returns a JWT token if authentication is successful.
     """
-    try:
-        token: str = auth_service.login(
-            username=form_data.username, password=form_data.password)
-        return TokenResponse(access_token=token, token_type="bearer")
-    except InvalidUserCredentialsException as e:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED, detail=str(e))
-    except UserOperationException as e:
-        raise HTTPException(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
-    except OperationFailedException as e:
-        raise HTTPException(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
-    except Exception as e:
-        raise HTTPException(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
+    token: str = auth_service.login(
+        username=form_data.username, password=form_data.password)
+    return TokenResponse(access_token=token, token_type="bearer")
 
 
 @auth_router.post(path="/register", status_code=status.HTTP_200_OK, response_model=TokenResponse)
@@ -41,21 +25,5 @@ async def register(auth_service: AuthService, user: UserRequest) -> TokenRespons
     """
     Register a new user and return a JWT token.
     """
-    try:
-        token: str = auth_service.register(user=user.to_orm())
-        return TokenResponse(access_token=token, token_type="bearer")
-    except UserAlreadyExistsException as e:
-        raise HTTPException(
-            status_code=status.HTTP_409_CONFLICT, detail=str(e))
-    except InvalidUserCredentialsException as e:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
-    except UserOperationException as e:
-        raise HTTPException(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
-    except OperationFailedException as e:
-        raise HTTPException(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
-    except Exception as e:
-        raise HTTPException(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
+    token: str = auth_service.register(user=user.to_orm())
+    return TokenResponse(access_token=token, token_type="bearer")
