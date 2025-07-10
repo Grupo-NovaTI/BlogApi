29088d0d3c2fd1d541d03905f2167f13209911af
Julio Adrian Ferreyra Tello <julioferreyra.dev@gmail.com>
2025-07-10 05:30:19 +0000

    Refactor tag repository and service to enhance exception handling with decorators for improved error management and consistency.

--- app/tags/repositories/tag_repository.py
+++ app/tags/repositories/tag_repository.py
@@ -1,28 +1,19 @@
 from typing import List, Optional
 from sqlalchemy.orm.session import Session
-from app.utils.errors.exception_handlers import handle_database_exception
 from app.tags.models.tag_model import TagModel
 from app.utils.logger.application_logger import ApplicationLogger
 from app.utils.enums.operations import Operations
 from sqlalchemy import or_
 _logger = ApplicationLogger(__name__)
 
-_MODEL = "Tag"
 class TagRepository:
     def __init__(self, db_session: Session) -> None:
         self._db_session: Session = db_session
 
-    @handle_database_exception(
-        model = _MODEL,
-        operation=Operations.FETCH
-    )
     def get_all_tags(self, limit: int = 10, offset: int = 0) -> List[TagModel]:
         return self._db_session.query(TagModel).limit(limit=limit).offset(offset=offset).all()
 
-    @handle_database_exception(
-        model = _MODEL,
-        operation=Operations.FETCH_BY
-    )
+ 
     def get_tag_by_id(self, tag_id: int) -> Optional[TagModel]:
         """
         Retrieve a tag by its ID from the database.
@@ -39,10 +30,7 @@ class TagRepository:
         return self._db_session.query(
                 TagModel).filter(TagModel.id == tag_id).first()
         
-    @handle_database_exception(
-         model = _MODEL,
-        operation=Operations.FETCH
-    )
+
     def get_tag_by_name(self, tag_name: str) -> Optional[TagModel]:
         """
         Retrieve a tag by its name from the database.
@@ -59,10 +47,6 @@ class TagRepository:
         return self._db_session.query(
             TagModel).filter(TagModel.name == tag_name).first()
     
-    @handle_database_exception(
-         model = _MODEL,
-        operation=Operations.CREATE
-    )
     def create_tag(self, tag: TagModel) -> TagModel:
         """
         Create a new tag in the database.
@@ -80,10 +64,8 @@ class TagRepository:
         self._db_session.commit()
         self._db_session.refresh(tag)
         return tag
-    @handle_database_exception(
-         model = _MODEL,
-        operation=Operations.UPDATE
-    )
+    
+    
     def update_tag(self, tag_id : int, tag_data: dict) -> Optional[TagModel]:
         """
         Update an existing tag in the database.
@@ -105,10 +87,6 @@ class TagRepository:
         self._db_session.commit()
         return self.get_tag_by_id(tag_id)
 
-    @handle_database_exception(
-         model = _MODEL,
-        operation=Operations.DELETE
-    )
     def delete_tag(self, tag_id: int) -> Optional[TagModel]:
         """
         Delete a tag by its ID from the database.
@@ -125,9 +103,7 @@ class TagRepository:
         self._db_session.delete(instance=target_tag)
         self._db_session.commit()
         return target_tag
-    @handle_database_exception(
-         model = _MODEL,
-        operation=Operations.FETCH
-    )
+
+
     def get_tag_by_id_or_name(self, tag_id: int, tag_name: str) -> Optional[TagModel]:
             return self._db_session.query(TagModel).filter(or_(TagModel.id == tag_id, TagModel.name == tag_name)).first()
--- app/tags/services/tag_service.py
+++ app/tags/services/tag_service.py
@@ -2,13 +2,17 @@ from typing import List, Optional
 from app.tags.models.tag_model import TagModel
 from app.tags.repositories.tag_repository import TagRepository
 from app.utils.errors.exceptions import NotFoundException as TagNotFoundException, AlreadyExistsException as TagAlreadyExistsException, ValidationException as TagInvalidException
-from app.utils.errors.error_messages import not_found_message, already_exists_message, validation_error_message
-
+from app.utils.errors.exception_handlers import handle_read_exceptions, handle_service_transaction
+from app.utils.enums.operations import Operations
+_MODEL_NAME = "Tags"    
 class TagService:
     def __init__(self, tag_repository: TagRepository) -> None:
         self._repository: TagRepository = tag_repository
-        self.model_name = "Tag"
 
+    @handle_read_exceptions(
+        model=_MODEL_NAME,
+        operation=Operations.FETCH
+    )
     def get_tags(self, limit: int, offset: int) -> List[TagModel]:
         """Retrieve all tags from the repository.
         Returns:
@@ -17,7 +21,10 @@ class TagService:
             TagOperationException: If there is an error retrieving tags from the repository.
         """
         return self._repository.get_all_tags(limit=limit, offset=offset)
-
+    @handle_read_exceptions(
+        model=_MODEL_NAME,
+        operation=Operations.FETCH_BY
+    )
     def get_tag_by_id(self, tag_id: int) -> Optional[TagModel]:
         """Retrieve a tag by its ID.
         Args:
@@ -29,6 +36,10 @@ class TagService:
         """
         return self._repository.get_tag_by_id(tag_id=tag_id)
 
+    @handle_service_transaction(
+        model=_MODEL_NAME,
+        operation=Operations.CREATE
+    )
     def create_tag(self, tag: TagModel) -> TagModel:
         """
         Create a new tag in the repository.
@@ -43,10 +54,14 @@ class TagService:
         check_tag_exists: Optional[TagModel] = self._repository.get_tag_by_name(
             tag_name=str(tag.name))
         if check_tag_exists:
-            raise TagAlreadyExistsException(identifier=str(tag.name), model=self.model_name)
+            raise TagAlreadyExistsException(identifier=str(tag.name), model=_MODEL_NAME)
 
         return self._repository.create_tag(tag=tag)
 
+    @handle_service_transaction(
+        model=_MODEL_NAME,
+        operation=Operations.UPDATE
+    )
     def update_tag(self, tag_data: dict, tag_id: int) -> TagModel:
         """
         Update an existing tag in the repository.
@@ -57,25 +72,29 @@ class TagService:
         """
         tag_name: Optional[str] = tag_data.get("name")
         if not tag_name:
-            raise TagInvalidException(identifier="name", model=self.model_name)
+            raise TagInvalidException(identifier="name", model=_MODEL_NAME)
         if not isinstance(tag_name, str):
             raise TagInvalidException(
                 identifier=str(tag_name),
-                model=self.model_name)
+                model=_MODEL_NAME)
         check_tag_exists_by_name: Optional[TagModel] = self._repository.get_tag_by_name(
             tag_name=str(tag_name))
         if check_tag_exists_by_name:
             raise TagAlreadyExistsException(
                 identifier=str(tag_name),
-                model=self.model_name,
+                model=_MODEL_NAME,
             )
 
         updated_tag: Optional[TagModel] = self._repository.update_tag(
             tag_data=tag_data, tag_id=tag_id)
         if not updated_tag:
-            raise TagNotFoundException(identifier=str(tag_id), model=self.model_name)
+            raise TagNotFoundException(identifier=str(tag_id), model=_MODEL_NAME)
         return updated_tag
 
+    @handle_service_transaction(
+        model=_MODEL_NAME,
+        operation=Operations.DELETE
+    )
     def delete_tag(self, tag_id: int):
         """
         Delete a tag from the repository.
@@ -87,9 +106,13 @@ class TagService:
         
         tag_result: Optional[TagModel] = self._repository.delete_tag(tag_id=tag_id)
         if not tag_result:
-            raise TagNotFoundException(identifier=str(tag_id), model=self.model_name)
+            raise TagNotFoundException(identifier=str(tag_id), model=_MODEL_NAME)
         return tag_result
 
+    @handle_read_exceptions(
+        model=_MODEL_NAME,
+        operation=Operations.FETCH_BY
+    )
     def get_tag_by_id_or_name(self, tag_id: int, tag_name: str) -> Optional[TagModel]:
         """
         Retrieve a tag by its ID or name.
@@ -103,6 +126,10 @@ class TagService:
         """
         return self._repository.get_tag_by_id_or_name(tag_id=tag_id, tag_name=tag_name)
 
+    @handle_read_exceptions(
+        model=_MODEL_NAME,
+        operation=Operations.FETCH_BY
+    )
     def get_tag_by_name(self, tag_name: str) -> Optional[TagModel]:
         """
         Retrieve a tag by its name.
