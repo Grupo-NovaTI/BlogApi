d72d1c8747265bd46bdf6109c7129c8df23f01e1
Julio Adrian Ferreyra Tello <julioferreyra.dev@gmail.com>
2025-07-17 03:53:38 +0000

    Refactor image validation: replace HTTPException with custom exceptions for file type and size validation

--- app/utils/validators/upload_image_validator.py
+++ app/utils/validators/upload_image_validator.py
@@ -1,13 +1,14 @@
 from typing import List
 
-from fastapi import HTTPException, UploadFile
-from starlette import status
+from fastapi import UploadFile
 
 from app.utils.constants.constants import (ALLOWED_MIME_TYPES,
                                            MAX_FILE_SIZE_BYTES)
+from app.utils.errors.exceptions import (InvalidFileTypeException,
+                                         RequestEntityTooLargeException)
 
 
-async def validate_uploaded_image(file: UploadFile, image_size : int = MAX_FILE_SIZE_BYTES, allowed_types: List[str] = ALLOWED_MIME_TYPES) -> bytes:
+async def validate_uploaded_image(file: UploadFile, image_size: int = MAX_FILE_SIZE_BYTES, allowed_types: List[str] = ALLOWED_MIME_TYPES) -> bytes:
     """
     Validates an uploaded profile picture for type and size.
 
@@ -21,20 +22,17 @@ async def validate_uploaded_image(file: UploadFile, image_size : int = MAX_FILE_
         HTTPException: If the file type or size is invalid.
     """
 
-    
-
-    if file.content_type not in allowed_types:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail=f"Invalid file type. Allowed types are: {', '.join(allowed_types)}"
+    if file.content_type is not None and file.content_type not in allowed_types:
+        raise InvalidFileTypeException(
+            allowed_types=allowed_types,
+            provided_type=file.content_type
         )
 
     file_content: bytes = await file.read()
 
     if len(file_content) > MAX_FILE_SIZE_BYTES:
-        raise HTTPException(
-            status_code=status.HTTP_413_REQUEST_ENTITY_TOO_LARGE,
-            detail=f"File size exceeds the limit of {image_size / (1024*1024):.0f}MB."
+        raise RequestEntityTooLargeException(
+            details=f"File size exceeds the limit of {image_size / (1024*1024):.0f}MB."
         )
 
     return file_content
